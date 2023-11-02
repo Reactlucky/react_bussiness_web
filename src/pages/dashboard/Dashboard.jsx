69743d4fc73308c06f6c2f478014ec93b15cf014
Lucky <asfaqeh@gmail.com>
2023-11-03 02:20:36 +0530

    TAB VIEW

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -14,6 +14,7 @@ import { Avatar } from "primereact/avatar";
 import arrow_up from "../../assets/images/arrow_up.svg";
 import arrow_down from "../../assets/images/arrow_down.svg";
 import InnerHeader from "../../comps/inner-header/InnerHeader";
+import General from "../customer/tabViewContact/General";
 
 const Dashboard = () => {
   const [showBarChart, setShowBarChart] = useState(false);
@@ -93,18 +94,10 @@ const Dashboard = () => {
           </div>
         </div>
         <TabView>
-          <TabPanel header="Header I">
-            <p className="m-0">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+          <TabPanel header="General">
+            <General/>
           </TabPanel>
-          <TabPanel header="Header II">
+          <TabPanel header="Projects">
             <p className="m-0">
               Sed ut perspiciatis unde omnis iste natus error sit voluptatem
               accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
@@ -115,7 +108,39 @@ const Dashboard = () => {
               velit, sed quia non numquam eius modi.
             </p>
           </TabPanel>
-          <TabPanel header="Header III">
+          <TabPanel header="Invoices">
+            <p className="m-0">
+              At vero eos et accusamus et iusto odio dignissimos ducimus qui
+              blanditiis praesentium voluptatum deleniti atque corrupti quos
+              dolores et quas molestias excepturi sint occaecati cupiditate non
+              provident, similique sunt in culpa qui officia deserunt mollitia
+              animi, id est laborum et dolorum fuga. Et harum quidem rerum
+              facilis est et expedita distinctio. Nam libero tempore, cum soluta
+              nobis est eligendi optio cumque nihil impedit quo minus.
+            </p>
+            </TabPanel>
+          <TabPanel header="Quotation">
+            <p className="m-0">
+              At vero eos et accusamus et iusto odio dignissimos ducimus qui
+              blanditiis praesentium voluptatum deleniti atque corrupti quos
+              dolores et quas molestias excepturi sint occaecati cupiditate non
+              provident, similique sunt in culpa qui officia deserunt mollitia
+              animi, id est laborum et dolorum fuga. Et harum quidem rerum
+              facilis est et expedita distinctio. Nam libero tempore, cum soluta
+              nobis est eligendi optio cumque nihil impedit quo minus.
+            </p>
+            </TabPanel>
+          <TabPanel header="Transactions">
+            <p className="m-0">
+              At vero eos et accusamus et iusto odio dignissimos ducimus qui
+              blanditiis praesentium voluptatum deleniti atque corrupti quos
+              dolores et quas molestias excepturi sint occaecati cupiditate non
+              provident, similique sunt in culpa qui officia deserunt mollitia
+              animi, id est laborum et dolorum fuga. Et harum quidem rerum
+              facilis est et expedita distinctio. Nam libero tempore, cum soluta
+              nobis est eligendi optio cumque nihil impedit quo minus.
+            </p></TabPanel>
+          <TabPanel header="Email">
             <p className="m-0">
               At vero eos et accusamus et iusto odio dignissimos ducimus qui
               blanditiis praesentium voluptatum deleniti atque corrupti quos
